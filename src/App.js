7fee689b99ef72ee3e80c4fcdd2f735045e6fa28
Christos Demertzis <picious@gmail.com>
2019-08-05 22:26:45 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    selectedFile: null
+  }
+  fileSelectedHandler = event => {
+    this.setState({selectedFile: event.target.files[0]});
+  }
+
+  fileUploadHandler = () => {
+    const fd = new FormData();
+    fd.append('image', this.state.selectedFile, this.state.selectedFile.name);
+    axios
+      .post('http://83.212.89.217:5000/predict', fd)
+      .then(response => {
+        console.log(response.data.predictions.label);
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input
+          style={{
+          display: 'none'
+        }}
+          type="file"
+          onChange={this.fileSelectedHandler}
+          ref={fileInput => this.fileInput = fileInput}/>
+        <button onClick={() => this.fileInput.click()}>Pick File</button>
+        <button onClick={this.fileUploadHandler}>Upload</button>
+
+      </div>
+    );
+  }
 }
 
 export default App;
